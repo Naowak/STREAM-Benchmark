5a649a025d101887e7ee064c3da9f7d945f951b2
Naowak <yannis.bendiouis@gmail.com>
2025-07-17 15:41:25 +0200

    add large tasks

--- stream_benchmark/evals.py
+++ stream_benchmark/evals.py
@@ -126,3 +126,66 @@ stream_medium = {
         'params': {"n_train": 1000, "n_valid": 200, "n_test": 1000, "path": "./data/mnist/", "cache_dir": "./data/"},
     },
 }
+
+stream_large = {
+    'sinus_forecasting': {
+        'fct': generate_sinus_forecasting,
+        'params': {"sequence_length": 20000, "forecast_length": 50, "training_ratio": 0.45, "validation_ratio": 0.1, "testing_ratio": 0.45},
+        'classification': False,
+    },
+    'chaotic_forecasting': {
+        'fct': generate_chaotic_forecasting,
+        'params': {"sequence_length": 20000, "forecast_length": 50, "training_ratio": 0.45, "validation_ratio": 0.1, "testing_ratio": 0.45},
+        'classification': False,
+    },
+    'discrete_postcasting': {
+        'fct': generate_discrete_postcasting,
+        'params': {"n_train": 10000, "n_valid": 2000, "n_test": 10000, "sequence_length": 300, "delay": 50, "n_symbols": 8},
+        'classification': True,
+    },
+    'continuous_postcasting': {
+        'fct': generate_continuous_postcasting,
+        'params': {"n_train": 10000, "n_valid": 2000, "n_test": 10000, "sequence_length": 300, "delay": 50},
+        'classification': False,
+    },
+    'discrete_pattern_completion': {
+        'fct': generate_discrete_pattern_completion,
+        'classification': True,
+        'params': {"n_train": 10000, "n_valid": 2000, "n_test": 10000, "sequence_length": 500, "n_symbols": 20, "base_length": 25, "mask_ratio": 0.2}, 
+    },
+    'continuous_pattern_completion': {
+        'fct': generate_continuous_pattern_completion,
+        'classification': False,
+        'params': {"n_train": 10000, "n_valid": 2000, "n_test": 10000, "sequence_length": 500, "base_length": 20, "mask_ratio": 0.2}, 
+    },
+    'bracket_matching': {
+        'fct': generate_bracket_matching,
+        'params': {"n_train": 10000, "n_valid": 2000, "n_test": 10000, "sequence_length": 300, "max_depth": 30},
+        'classification': True,
+    },
+    'simple_copy': {
+        'fct': generate_simple_copy,
+        'classification': True,
+        'params': {"n_train": 10000, "n_valid": 2000, "n_test": 10000, "sequence_length": 150, "delay": 30, "n_symbols": 20}, 
+    },
+    'selective_copy': {
+        'fct': generate_selective_copy,
+        'classification': True,
+        'params': {"n_train": 10000, "n_valid": 2000, "n_test": 10000, "sequence_length": 240, "delay": 30, "n_markers": 30, "n_symbols": 20},
+    },
+    'adding_problem': {
+        'fct': generate_adding_problem,
+        'classification': True,
+        'params': {"n_train": 10000, "n_valid": 2000, "n_test": 10000, "sequence_length": 50, "max_number": 20},
+    },
+    'sorting_problem': {
+        'fct': generate_sorting_problem,
+        'classification': True,
+        'params': {"n_train": 10000, "n_valid": 2000, "n_test": 10000, "sequence_length": 50, "n_symbols": 20}, 
+    },
+    'sequential_mnist': {
+        'fct': generate_sequential_mnist,
+        'classification': True,
+        'params': {"n_train": 10000, "n_valid": 2000, "n_test": 10000, "path": "./data/mnist/", "cache_dir": "./data/"},
+    },
+}
